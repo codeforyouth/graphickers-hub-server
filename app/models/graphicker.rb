21c64be89981089e7c98503fda05abcf6b20352b
sensu-watson <sensunowatson@gmail.com>
2019-10-06 16:50:26 +0900

    wrong comment fix

--- app/models/graphicker.rb
+++ app/models/graphicker.rb
@@ -1,6 +1,6 @@
 class Graphicker < ApplicationRecord
-  has_secure_token # add by bcrypt. use password, password_confirmation
-  has_secure_password
+  has_secure_token
+  has_secure_password # add by bcrypt. use password, password_confirmation
   has_one :image
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
