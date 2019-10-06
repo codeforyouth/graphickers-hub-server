acecc2d67a2cb886eae71fe7dd5de834fb033717
sensu-watson <sensunowatson@gmail.com>
2019-10-06 21:09:44 +0900

    add test for create new graphicker

--- spec/factories/graphickers.rb
+++ spec/factories/graphickers.rb
@@ -1,8 +1,29 @@
 FactoryBot.define do
     factory :graphicker do
-        sequence(:name) { |n| "TEST_NAME#{n}"}
-        sequence(:email) { |n| "testmail_#{n}@test.com"}
-        sequence(:password) { |n| "hogehoge#{n}"}
-        sequence(:password_confirmation) { |n| "hogehoge#{n}"}
+        name {'watson'}
+        email {'sensunowatson@myemail.com'}
+        password {'hogehoge'}
+        password_confirmation {'hogehoge'}
+
+        trait :no_name do
+            name {nil}
+        end
+
+        trait :no_email do
+            email {nil}
+        end
+
+        trait :invalid_email do
+            email {'sensunowatson-myemail.com'}
+        end
+
+        trait :password_too_short do
+            password {'a'}
+            password_confirmation {'a'}
+        end
+
+        trait :password_unmatch do
+            password_confirmation {'hogehoge2'}
+        end
     end
 end
--- spec/requests/graphickers_spec.rb
+++ spec/requests/graphickers_spec.rb
@@ -1,19 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe 'Graphicker' do
-    it '新しいグラフィッカーが新規登録する' do
-        valid_params = {
-            name: 'watson',
-            email: 'sensunowatson@myemail.com',
-            password: 'hogehoge',
-            password_confirmation: 'hogehoge'
-        }
+    context 'パラメータが妥当な場合' do
+        it 'リクエストが成功すること' do
+            post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker)}
+            expect(response.status).to eq(201)
+        end
+        it 'グラフィッカーが登録されること' do
+            expect do
+                post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker)}
+            end.to change(Graphicker, :count).by(1)
+        end 
+    end
 
-        expect {
-            post '/graphickers',
-            params: { graphicker: valid_params }
-        }.to change(Graphicker, :count).by(+1)
+    context 'nameが与えられていない場合' do
+        it 'リクエストが失敗すること' do
+            post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker, :no_name)}
+            expect(response.status).to eq(422)
+        end
 
-        expect(response.status).to eq(201)
-    end 
+        it 'グラフィッカーが登録されないこと' do
+            expect do
+                post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker, :no_name)}
+            end.to_not change(Graphicker, :count)
+        end
+    end
+
+    context 'emailが与えられていない場合' do
+        it 'リクエストが失敗すること' do
+            post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker, :no_email)}
+            expect(response.status).to eq(422)
+        end
+
+        it 'グラフィッカーが登録されないこと' do
+            expect do
+                post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker, :no_email)}
+            end.to_not change(Graphicker, :count)
+        end
+    end
+
+    context 'emailが不正な場合' do
+        it 'リクエストが失敗すること' do
+            post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker, :invalid_email)}
+            expect(response.status).to eq(422)
+        end
+
+        it 'グラフィッカーが登録されないこと' do
+            expect do
+                post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker, :invalid_email)}
+            end.to_not change(Graphicker, :count)
+        end
+    end
+
+    context 'passwordが短すぎる場合' do
+        it 'リクエストが失敗すること' do
+            post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker, :password_too_short)}
+            expect(response.status).to eq(422)
+        end
+
+        it 'グラフィッカーが登録されないこと' do
+            expect do
+                post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker, :password_too_short)}
+            end.to_not change(Graphicker, :count)
+        end
+    end
+
+    context 'passwordが不一致な場合' do
+        it 'リクエストが失敗すること' do
+            post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker, :password_unmatch)}
+            expect(response.status).to eq(422)
+        end
+
+        it 'グラフィッカーが登録されないこと' do
+            expect do
+                post '/graphickers', params: {graphicker: FactoryBot.attributes_for(:graphicker, :password_unmatch)}
+            end.to_not change(Graphicker, :count)
+        end
+    end
 end
