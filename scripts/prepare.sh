42a9e2b5c608367a26e3bd76ba65b8a2ba60c258
sensu-watson <sensunowatson@gmail.com>
2020-05-10 22:22:35 +0900

    add profile

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source /etc/profile
+
 cd /var/www/rails
 bundle install
 rails db:create RAILS_ENV=production
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+source /etc/profile
+
 cd /var/www/rails
 rails server -e production
